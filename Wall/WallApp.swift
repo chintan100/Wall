cf5a5e245fc9edec6827f002d9174e80ce4a33ca
Chintan Patel <chintan100@hotmail.com>
2025-05-24 12:02:41 +0530

    Initial Commit

---
+++ Wall/WallApp.swift
@@ -0,0 +1,17 @@
+//
+//  WallApp.swift
+//  Wall
+//
+//  Created by Chintan Patel on 24/05/25.
+//
+
+import SwiftUI
+
+@main
+struct WallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WallTests/WallTests.swift
@@ -0,0 +1,17 @@
+//
+//  WallTests.swift
+//  WallTests
+//
+//  Created by Chintan Patel on 24/05/25.
+//
+
+import Testing
+@testable import Wall
+
+struct WallTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
